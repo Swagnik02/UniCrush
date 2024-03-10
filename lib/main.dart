558276927748f2de0916b2c71f11e8bf30172ac3
Swagnik02 <swagnik1234@gmail.com>
2024-03-10 16:21:06 +0530

    refactored

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,6 @@ import 'package:flutter/services.dart';
 import 'package:layout/layout.dart';
 import 'package:unicrush/firebase_options.dart';
 import 'package:unicrush/presentation/route/routes.dart';
-import 'package:unicrush/presentation/views/auth/login/login_page.dart';
-import 'package:unicrush/presentation/views/auth/onboard/onboard_page.dart';
-import 'package:unicrush/presentation/views/auth/signup/signup_page.dart';
-import 'package:unicrush/presentation/views/find/find_page.dart';
-import 'package:unicrush/presentation/views/home/home_page.dart';
-import 'package:unicrush/presentation/views/matches/matches_page.dart';
-import 'package:unicrush/presentation/views/profile/profile_page.dart';
 
 import 'presentation/utils/extensions/extensions.dart';
 
@@ -23,7 +16,7 @@ void main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  Get.put(ThemeController()); // Initialize the theme controller
+  Get.put(ThemeController());
 
   runApp(MyApp());
 }
@@ -45,31 +38,3 @@ class MyApp extends StatelessWidget {
 
   final String initialRoute = Routes.home;
 }
-
-final List<GetPage> routes = [
-  // auth
-  GetPage(name: Routes.onboard, page: () => OnboardPage()),
-  GetPage(name: Routes.login, page: () => LoginPage()),
-  GetPage(name: Routes.signup, page: () => SignUpPage()),
-
-  // bottom nav
-  GetPage(name: Routes.home, page: () => HomePage()),
-
-  // main pages
-  GetPage(name: Routes.matches, page: () => MatchesPage()),
-  GetPage(name: Routes.find, page: () => FindPage()),
-  GetPage(name: Routes.profile, page: () => ProfilePage()),
-];
-
-
-
-/* 
-
-
-    TextStyle? titleTextStyle = context.adaptive(
-      Theme.of(context).textTheme.titleMedium,
-      Theme.of(context).textTheme.headlineSmall,
-      md: Theme.of(context).textTheme.titleLarge,
-    );
-
-    */
--- lib/presentation/route/routes.dart
+++ lib/presentation/route/routes.dart
@@ -1,3 +1,12 @@
+import 'package:get/get.dart';
+import 'package:unicrush/presentation/views/auth/login/login_page.dart';
+import 'package:unicrush/presentation/views/auth/onboard/onboard_page.dart';
+import 'package:unicrush/presentation/views/auth/signup/signup_page.dart';
+import 'package:unicrush/presentation/views/find/find_page.dart';
+import 'package:unicrush/presentation/views/home/home_page.dart';
+import 'package:unicrush/presentation/views/matches/matches_page.dart';
+import 'package:unicrush/presentation/views/profile/profile_page.dart';
+
 class Routes {
   static const String login = '/login';
   static const String signup = '/signup';
@@ -7,3 +16,18 @@ class Routes {
   static const String find = '/find';
   static const String profile = '/profile';
 }
+
+final List<GetPage> routes = [
+  // auth
+  GetPage(name: Routes.onboard, page: () => OnboardPage()),
+  GetPage(name: Routes.login, page: () => LoginPage()),
+  GetPage(name: Routes.signup, page: () => SignUpPage()),
+
+  // bottom nav
+  GetPage(name: Routes.home, page: () => HomePage()),
+
+  // main pages
+  GetPage(name: Routes.matches, page: () => MatchesPage()),
+  GetPage(name: Routes.find, page: () => FindPage()),
+  GetPage(name: Routes.profile, page: () => ProfilePage()),
+];
