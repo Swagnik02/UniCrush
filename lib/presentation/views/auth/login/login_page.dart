5ca9e0a5081df69469d93248b3882d286031dba1
Swagnik02 <swagnik1234@gmail.com>
2024-03-10 15:09:20 +0530

    loginPage controller added

--- lib/presentation/views/auth/login/login_page.dart
+++ lib/presentation/views/auth/login/login_page.dart
@@ -3,125 +3,129 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:unicrush/presentation/configs/configs.dart';
+import 'package:unicrush/presentation/views/auth/login/login_page_controller.dart';
 import 'package:unicrush/presentation/views/auth/signup/signup_page.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+  final LoginPageController controller = Get.put(LoginPageController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBackground,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 50.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Login',
-                        style: textStyleTitle,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Enter your credentials to \ncontinue.',
-                        textAlign: TextAlign.center,
-                        style: textStyleSubTitle,
-                      ),
-                    ],
-                  ),
-                ],
+    return GetBuilder<LoginPageController>(
+      builder: (_) => Scaffold(
+        backgroundColor: kBackground,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Login',
+                          style: textStyleTitle,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Enter your credentials to \ncontinue.',
+                          textAlign: TextAlign.center,
+                          style: textStyleSubTitle,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            CustomTextField(
-              controller: TextEditingController(),
-              hint: 'Enter your Email-id',
-              keyboardType: TextInputType.emailAddress,
-              label: 'Email',
-            ),
-            CustomTextField(
-              controller: TextEditingController(),
-              hint: 'Enter your password',
-              keyboardType: TextInputType.emailAddress,
-              label: 'Password',
-              isPassword: true,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Text('Login'),
+              CustomTextField(
+                controller: controller.emailController,
+                hint: 'Enter your Email-id',
+                keyboardType: TextInputType.emailAddress,
+                label: 'Email',
+              ),
+              CustomTextField(
+                controller: controller.passwordController,
+                hint: 'Enter your password',
+                keyboardType: TextInputType.emailAddress,
+                label: 'Password',
+                isPassword: true,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: controller.login,
+                        child: const Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Text('Login'),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  decoration: BoxDecoration(color: Colors.grey),
-                  width: Get.width / 4,
-                  height: 2,
-                ),
-                Text('Or Sign in With'),
-                Container(
-                  decoration: BoxDecoration(color: Colors.grey),
-                  width: Get.width / 4,
-                  height: 2,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _socialIconBtn(FontAwesomeIcons.facebook),
-                _socialIconBtn(FontAwesomeIcons.google),
-              ],
-            ),
-            RichText(
-              text: TextSpan(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  TextSpan(
-                    text: 'Don\'t Have an Account? ',
-                    style: textStyleSubTitle2,
+                  Container(
+                    decoration: const BoxDecoration(color: Colors.grey),
+                    width: Get.width / 4,
+                    height: 2,
                   ),
-                  TextSpan(
-                    text: 'Sign Up',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.blue,
-                    ),
-                    recognizer: TapGestureRecognizer()
-                      ..onTap = () {
-                        Get.to(SignUpPage());
-                        // Handle Sign Up onTap action here
-                        print('Sign Up tapped!');
-                      },
+                  const Text('Or Sign in With'),
+                  Container(
+                    decoration: const BoxDecoration(color: Colors.grey),
+                    width: Get.width / 4,
+                    height: 2,
                   ),
                 ],
               ),
-            ),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _socialIconBtn(FontAwesomeIcons.facebook),
+                  _socialIconBtn(FontAwesomeIcons.google),
+                ],
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Don\'t Have an Account? ',
+                      style: textStyleSubTitle2,
+                    ),
+                    TextSpan(
+                      text: 'Sign Up',
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.blue,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Get.to(const SignUpPage());
+                          // Handle Sign Up onTap action here
+                          print('Sign Up tapped!');
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -129,9 +133,9 @@ class LoginPage extends StatelessWidget {
 
   Widget _socialIconBtn(IconData icon) {
     return Padding(
-      padding: EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(8.0),
       child: Container(
-        padding: EdgeInsets.all(15),
+        padding: const EdgeInsets.all(15),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50),
           color: Colors.white,
@@ -170,20 +174,21 @@ class CustomTextField extends StatelessWidget {
         keyboardType: keyboardType,
         obscureText: isPassword ?? false,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 30, vertical: 16),
           labelText: label,
           hintText: hint,
           hintStyle: textStyleSubTitle,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30.0),
-            borderSide: BorderSide(
+            borderSide: const BorderSide(
               color: Colors.grey,
               width: 1.0,
             ),
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30.0),
-            borderSide: BorderSide(
+            borderSide: const BorderSide(
               color: Colors.grey,
               width: 1.0,
             ),
